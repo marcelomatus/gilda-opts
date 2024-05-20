87659e71a3ec20f389ae61f4b3b8b3454e9f4222
Marcelo Matus <marcelomatusacuna@gmail.com>
2024-05-20 11:58:00 -0400

    adding linear_problem model

---
+++ src/gilda_opts/linear_problem.py
@@ -0,0 +1,201 @@
+import numpy as np
+import pyomo.environ as pyo # type: ignore
+import pyomo.kernel as pmo # type: ignore
+from pyomo.environ import SymbolMap # type: ignore
+from scipy.sparse import dok_matrix
+
+
+class LinearProblem:
+    inf = float("inf")
+
+    def __init__(self, scale_obj=1.0, solver='cbc', integer_mode=True):
+        self.scale_obj = scale_obj
+        self.solver = solver
+        self.integer_mode = integer_mode
+
+        self.A = {}
+        self.c = {}
+        self.clb = {}
+        self.cub = {}
+        self.rlb = {}
+        self.rub = {}
+
+        self.rnames = {}
+        self.cnames = {}
+        self.ctypes = {}
+
+        self.rpos = {}
+        self.cpos = {}
+
+        self.cols = 0
+        self.rows = 0
+
+    def add_col(self, name=None, lb=0, ub=inf, c=0, dtype=0):
+        j = self.cols
+        self.clb[j] = lb
+        self.cub[j] = ub
+        self.c[j] = c
+        self.ctypes[j] = dtype
+        self.cnames[j] = name
+        self.cpos[name] = j
+
+        self.cols += 1
+        return j
+
+    def add_row(self, row={}, name=None, lb=-inf, ub=inf):
+        i = self.rows
+        for j, v in row.items():
+            self.A[i, j] = v
+
+        self.rlb[i] = lb
+        self.rub[i] = ub
+        self.rnames[i] = name
+        self.rpos[name] = i
+
+        self.rows += 1
+        return i
+
+    def add_rhs_row(self, row={}, name=None, rhs=0):
+        return self.add_row(row, name=name, lb=rhs, ub=rhs)
+
+    def set_coeff(self, i, j, v):
+        self.A[i, j] = v
+
+    def set_objc(self, j, cv):
+        self.c[j] = cv
+
+    def set_col_lb(self, j, clb):
+        self.clb[j] = clb
+
+    def set_col_ub(self, j, cub):
+        self.cub[j] = cub
+
+    def set_col(self, j, value):
+        self.clb[j] = value
+        self.cub[j] = value
+
+    def get_row_rhs(self, i):
+        return self.rlb.get(i, 0)
+
+    def set_row_rhs(self, i, v):
+        self.rlb[i] = v
+        self.rub[i] = v
+
+    def sparse_model(self):
+        A = dok_matrix((self.rows, self.cols))
+        for ij, v in self.A.items():
+            if v != 0:
+                A[ij] = v
+
+        rlb = np.ndarray(self.rows, dtype=float)
+        for k, v in self.rlb.items():
+            rlb[k] = v
+
+        rub = np.ndarray(self.rows, dtype=float)
+        for k, v in self.rub.items():
+            rub[k] = v
+
+        clb = np.ndarray(self.cols, dtype=float)
+        for k, v in self.clb.items():
+            clb[k] = v
+
+        cub = np.ndarray(self.cols, dtype=float)
+        for k, v in self.cub.items():
+            cub[k] = v
+
+        c = np.ndarray(self.cols, dtype=float)
+        for k, v in self.c.items():
+            c[k] = v
+
+        return A, rlb, rub, clb, cub, c
+
+    def pyomo_model(self):
+        m = pmo.block()
+        m.rows = range(self.rows)
+        m.cols = range(self.cols)
+        m.A, m.rlb, m.rub, m.clb, m.cub, m.c = self.sparse_model()
+
+        m.x = pmo.variable_list()
+        for j in m.cols:
+            dtype = (pmo.IntegerSet
+                     if self.integer_mode and self.ctypes[j] else pmo.RealSet)
+            xj = pmo.variable(
+                lb=m.clb[j],
+                ub=m.cub[j],
+                domain_type=dtype,
+            )
+            m.x.append(xj)
+
+        m.dual = pmo.suffix(direction=pmo.suffix.IMPORT)
+
+        m.constraints = pmo.matrix_constraint(m.A, lb=m.rlb, ub=m.rub, x=m.x)
+
+        scale = 1.0 / self.scale_obj
+        m.obj = pmo.objective(expr=sum(
+            (scale * m.c[col]) * m.x[col] for col in m.cols))
+
+        m.cnames = self.cnames
+        m.rnames = self.rnames
+
+        m.cpos = self.cpos
+        m.rpos = self.rpos
+
+        return m
+
+    def solve(
+        self,
+        solver=None,
+        keepfiles=False,
+        solver_options=None,
+    ):
+        self.model = self.pyomo_model()
+
+        solver = solver if solver is not None \
+            else self.solver
+
+        self.solver = pmo.SolverFactory(solver)
+        options_string = solver_options if solver_options is not None else ""
+        self.result = self.solver.solve(self.model,
+                                        options_string=options_string,
+                                        keepfiles=keepfiles)
+
+        return self.result
+
+    def get_col_ub(self, j):
+        return self.cub[j]
+
+    def get_col_lb(self, j):
+        return self.clb[j]
+
+    def get_obj(self):
+        return pyo.value(self.model.obj) * self.scale_obj
+
+    def get_status(self):
+        return self.result.solver.status
+
+    def get_time(self):
+        return self.result.solver.time
+
+    def get_col_at(self, j):
+        return pyo.value(self.model.x[j])
+
+    def get_col_sol(self, jj):
+        x = np.ndarray(len(jj))
+        for k, j in enumerate(jj):
+            x[k] = self.get_col_at(j)
+
+        return x
+
+    def get_dual_at(self, i):
+        return pyo.value(
+            self.model.dual[self.model.constraints[i]]) * self.scale_obj
+
+    def get_dual_sol(self, ii):
+        y = np.ndarray(len(ii))
+        for k, i in enumerate(ii):
+            y[k] = self.get_dual_at(i)
+
+        return y
+
+    def get_obj(self):
+        return float(pyo.value(self.model.obj) * self.scale_obj)
---
+++ tests/test_linear_problem.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+"""Tests for `linear_problem` package."""
+
+from math import isclose
+
+import numpy as np
+import pyomo.environ as pyo
+from scipy.optimize import linprog
+
+from gilda_opts import linear_problem as lpm
+ 
+
+def test_lp_1():
+    lp = lpm.LinearProblem(scale_obj=1.0, solver='cbc')
+
+    x = lp.add_col("x", c=-1)
+    assert x == 0
+
+    y = lp.add_col("y", c=-2)
+    assert y == 1
+
+    r1 = {}
+    r1[0] = 2
+    r1[1] = 1
+
+    c1 = lp.add_row(r1, ub=20)
+    assert c1 == 0
+
+    r2 = {}
+    r2[0] = -4
+    r2[1] = 5
+
+    c2 = lp.add_row(r2, ub=10)
+    assert c2 == 1
+
+    r3 = {}
+    r3[0] = 1
+    r3[1] = -2
+
+    c3 = lp.add_row(r3, ub=2)
+    assert c3 == 2
+
+    assert lp.cols == 2
+    assert lp.rows == 3
+
+    A, rlb, rub, cul, cub, c = lp.sparse_model()
+
+    assert np.array_equal(A.toarray(), np.array([[2, 1], [-4, 5], [1, -2]]))
+
+    assert np.array_equal(rub, np.array([20, 10, 2]))
+
+    lp.solve(keepfiles=False)
+
+    rel_tol = 1e-6
+    assert isclose(lp.get_obj(), -20.7142856, rel_tol=rel_tol)
+
+    #
+    # scipy solution
+    #
+    obj = [-1, -2]
+    #      ─┬  ─┬
+    #       │   └┤ Coefficient for y
+    #       └────┤ Coefficient for x
+
+    lhs_ineq = [
+        [2, 1],  # Red constraint left side
+        [-4, 5],  # Blue constraint left side
+        [1, -2]
+    ]  # Yellow constraint left side
+
+    rhs_ineq = [
+        20,  # Red constraint right side
+        10,  # Blue constraint right side
+        2
+    ]  # Yellow constraint right side
+
+    bnd = [
+        (0, float("inf")),  # Bounds of x
+        (0, float("inf"))
+    ]  # Bounds of y
+
+    opt = linprog(c=obj,
+                  A_ub=lhs_ineq,
+                  b_ub=rhs_ineq,
+                  bounds=bnd,
+                  method="revised simplex")
+
+    assert isclose(lp.get_obj(), opt.fun, rel_tol=rel_tol)
+
+    m = lp.model
+
+    assert len(m.x) == 2
+    assert isclose(pyo.value(m.x[0]), opt.x[0], rel_tol=rel_tol)
+    assert isclose(pyo.value(m.x[1]), opt.x[1], rel_tol=rel_tol)
+
+    assert len(m.constraints) == 3
+    assert isclose(pyo.value(m.dual[m.constraints[0]]),
+                   -0.928571428571429,
+                   rel_tol=rel_tol)
+    assert isclose(pyo.value(m.dual[m.constraints[1]]),
+                   -0.214285714285714,
+                   rel_tol=rel_tol)
