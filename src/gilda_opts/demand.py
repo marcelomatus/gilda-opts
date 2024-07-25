822a88abb8fb4501a19ea6fb9ed9356c04d99bba
Pilar Buitrago <159559681+mpbuitrago@users.noreply.github.com>
2024-07-25 17:31:03 -0500

    Update demand.py

--- src/gilda_opts/demand.py
+++ src/gilda_opts/demand.py
@@ -8,18 +8,19 @@ from gilda_opts.utils import NumberSched
 
 @dataclass
 class Demand(BaseClassJson):
-    """
-    Demand represents the base electric load in the scheduling problem.
-
-    Attributes:
-     uid:         Load unique id
-     name:        Load name
-     bus_uid:     Bus uid to be connected to
-     load_sched:  Load schedule [KW]
-    """
-
+    """Demand represents the base electric load in the scheduling problem."""
+    
+    #: Load unique id
     uid: int = -1
+
+    #: Load name
     name: str = ""
+
+    #: Bus uid to be connected to
     bus_uid: int = -1
+
+    #: Load schedule [KW]
     load_sched: NumberSched = 0.0
+
+    #: Cost of fail to schedule load
     cfail_sched: NumberSched = -1
