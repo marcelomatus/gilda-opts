c6074bcd27dacf1e8ac7cc56996dac1a6bb6e95e
Pilar Buitrago <159559681+mpbuitrago@users.noreply.github.com>
2024-08-01 17:06:29 -0500

    Update bess.py

--- src/gilda_opts/bess.py
+++ src/gilda_opts/bess.py
@@ -7,7 +7,7 @@ from gilda_opts.battery import Battery
 @dataclass
 class BESS(Battery):
     """Battery energy storage systems(BESS), allow energy generated from renewable sources such as solar and wind to be stored 
-    and later used during periods of high demand , helping to reduce costs and ensure a continuous electricity supply.
+    and later used during periods of high demand, helping to reduce costs and ensure a continuous electricity supply.
     
     BESS are significantly more sophisticated than typical batteries. Advanced software algorithms manage energy production, 
     while computerized control systems determine the optimal times to store or release energy to the grid. """
